301e1e9b45a4c5562288c81451f378c908fa84ba
GabeFansh <94994116+GabeFansh@users.noreply.github.com>
2024-02-06 16:14:20 -0500

    website-valentines

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import "./Button.css";
+
+
+const ButtonExample = () => {
+    const [isNoPressed, setIsNoPressed] = useState(false);
+    const [pressCount, setPressCount] = useState(0); // Add pressCount state variable
+    const [isElementsVisible, setIsElementsVisible] = useState(true); // Add isElementsVisible state variable
+
+    const handleNoButtonClick = () => {
+        if (pressCount < 20) { // Limit pressCount to 5
+            setIsNoPressed(true);
+            setPressCount(prevCount => prevCount + 1); // Increase pressCount by 1
+        }
+    };
+
+    const handleYesButtonClick = () => {
+        setPressCount(prevCount => prevCount + 1);
+        setIsElementsVisible(false); // Hide elements when Yes button is clicked
+    };
+
+    const yesButtonStyle = {
+        fontSize: isNoPressed ? `${16 * (pressCount + 1)}px` : '16px', // Update fontSize based on pressCount
+    };
+
+    return (
+        <div>
+            {isElementsVisible && (
+                <>
+                    <button className="blank-button" onClick={handleNoButtonClick}>No</button>
+                    <button className="blank-button" style={yesButtonStyle} onClick={handleYesButtonClick}>Yes</button>
+                </>
+            )}
+            {!isElementsVisible && <p>Yippee</p>}
+        </div>
+    );
+};
+
+export default ButtonExample;
